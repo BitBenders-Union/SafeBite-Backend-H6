47316d8d4a34325dc6e8b6d973136d85b9438b88
Jonas Østergaard <Jonasoe@hotmail.com>
2026-04-24 19:20:39 +0200

    small refactoring

--- SafeBite-Backend-H6.API/Contracts/Responses/CustomInfoResponse.cs
+++ SafeBite-Backend-H6.API/Contracts/Responses/CustomInfoResponse.cs
@@ -2,9 +2,9 @@
 
 public class CustomInfoResponse
 {
+    public required string UserId { get; set; }
     public required string Email { get; set; }
     public required bool IsEmailConfirmed { get; set; }
-    public required string UserId { get; set; }
     public required IList<string> Roles { get; set; }
     public required bool IsDeactivated { get; set; }
     public DateTime? DeactivatedTime { get; set; }
