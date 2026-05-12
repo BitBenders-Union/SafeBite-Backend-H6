06e26160fc4848e7b3c8ba27337a3b9e0a5ad3b7
ChimpChonk <sanjit.poudel28@gmail.com>
2026-05-12 13:42:52 +0200

    added health check, removed Dev from prod

---
+++ SafeBite-Backend-H6.API/Interfaces/Services/IHealthCheckService.cs
@@ -0,0 +1,6 @@
+﻿namespace SafeBite_Backend_H6.API.Interfaces.Services;
+
+public interface IHealthCheckService
+{
+    public Task<Boolean> HealthCheckPass();
+}
---
+++ SafeBite-Backend-H6.API/Services/HealthCheckService.cs
@@ -0,0 +1,22 @@
+﻿namespace SafeBite_Backend_H6.API.Services;
+
+public class HealthCheckService : IHealthCheckService
+{
+    private readonly IAllergyRepository _allergyRepository;
+
+    public HealthCheckService(IAllergyRepository allergyRepository)
+    {
+        _allergyRepository = allergyRepository;
+    }
+
+    //This is hardcoded because it's only used for API health check.
+    public async Task<bool> HealthCheckPass()
+    {
+        var response = await _allergyRepository.GetAllergyByNameAsync("Gluten"); //Hardcoded seeded allergy to check if we have connection to DB too.
+
+        if (response == null)
+            return false;
+
+        return true;
+    }
+}
